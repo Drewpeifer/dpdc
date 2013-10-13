f55a9f7d032d173c8b6b92c2e6f7c61f89914b72
Drew Peifer <peiftronica@gmail.com>
2013-10-13 01:17:06 -0400

    commented scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-	$('#nav div').hide(),
-	$('header p').hide(),
-	$('#wrapper').fadeIn(),
+	$('#nav div').hide(), // hide subsections
+	$('header p').hide(), // hide hypnoText
+	$('#wrapper').fadeIn(), // fade everything else in
 	$('#spiral').css({
-				transformOrigin: '120px 120px'
+				transformOrigin: '120px 120px' // center transform
 			})
 			.transition({
-				rotate:'360deg'
+				rotate:'360deg' // do one slow spin
 			}, 20000).stop(),
 
+	// hynpoText fades in header
 	$('p.first').fadeIn(6000).fadeOut(2000),
 	$('p.second').fadeIn(10000).fadeOut(2000),
 	$('p.third').fadeIn(15000).fadeOut(4000);
 }),
 
+// subsection / drawer functionality
 $('ul li').click(function() {
-	var activeTab = $(this).attr('id'),
-		activeHypnoText = $('header p.' + activeTab),
+	var activeTab = $(this).attr('id'), // clicked tab
+		activeHypnoText = $('header p.' + activeTab), // corresponding hypnoText
 		allHypnoText = $('header p');
 
 	if ($(this).hasClass('active')) { // when clicking an already-open tab
