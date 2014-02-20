cc46b1f3a6ff6fdcb20af1645b73c598e3e3ed30
Drew Peifer <peiftronica@gmail.com>
2014-02-20 00:12:00 -0500

    fixed firefox bug by chaining and separating animations, lowered star count

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(function() {
 	var star = '<div class="star"></div>',
 		starField = $('#starfield');
 
-	for (var i = 0; i < 20; i++) {
+	for (var i = 0; i < 15; i++) {
 
 		randoTop = Math.floor(Math.random() * 100),
 		randoLeft = Math.floor(Math.random() * 100),
@@ -20,19 +20,24 @@ $(function() {
 					'-moz-box-shadow':    '0 0 10px 3px ' + randomColor,
 					'-webkit-box-shadow': '0 0 10px 3px ' + randomColor,
 					'box-shadow':         '0 0 10px 3px ' + randomColor
-			   }).addClass('' + i + ''),
-
-			   console.log(randomColor);
+			   }).addClass('' + i + '');
 	}
+
+	console.log('stars are built')
 
 	$('button').click(function() {
 		$('.star').transition({
 			width: '+=300',
 			duration:3000
-		}).transition({
-			left:'+=100',
+		}),
+		starField.transition({
+			x:'-=30',
 			duration: 3000,
 			easing: 'snap'
+		}).transition({
+			x:'+=1050',
+			duration: 600
 		});
-	});
+	})
+
 });
