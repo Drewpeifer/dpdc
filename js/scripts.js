bde9fb79ed4d2959c93f2d1ca13a398b3f0631e5
Drew Peifer <peiftronica@gmail.com>
2014-03-03 04:44:39 -0500

    commented heavily in js, removed some old variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,44 @@
-var star = '<div class="new star"></div>',
-	allStars = $('.star'),
-	starField = $('#starfield'),
+var star = '<div class="new star"></div>', // generic baby star
+	allStars = $('.star'), // every star
+	starField = $('#starfield'), // wrapper of allStars
 	colorA = '#fff',// white
 	colorB = '#ffffee',// yellow
 	colorC = '#ccffff',// blue
 	colors = [colorA, colorB, colorC]
-	starCount = 1;
+	starCount = 1; // for debugging
 
+// starCrawl() sets the newest star created into motion
 function starCrawl(starField) {
 	var position = $('.new.star').offset(),
 		starLeft = position.left,
 		fieldWidth = $('#starfield').width();
 
-	$('.new.star').removeClass('new')
+	$('.new.star').removeClass('new') // star becomes an adult
 				  .animate({
-			x:((fieldWidth + 100) - starLeft) // 100 is arbitrary
+			x:(fieldWidth) // move it offscreen
 		}, {
 			duration:5000,
 			easing: 'linear',
 			step: function(now) {
-				if ((now + starLeft) >= fieldWidth) {
-					console.log('its gone now'),
-					$(this).stop()
-						   .remove(),
-					makeOneStar()
+				if ((now + starLeft) >= fieldWidth) { // when star goes offscreen
+					// console.log('a star has died'),
+					$(this).stop() // stop animation
+						   .remove(), // implode this star
+					makeOneStar() // make a new star
 				} else {
-					// never do anything here
+					// never do anything here,
+					// or your browser may explode
 				}
 			}
 		});
 }
 
+// makeOneStar() does exactly what it sounds like,
+// creating a new star with a random color and
+// vertical position
 function makeOneStar() {
 
 	randoTop = Math.floor(Math.random() * 100),
-	randoLeft = Math.floor(Math.random() * 100),
 	randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 	$(star).appendTo(starField)
@@ -48,13 +52,16 @@ function makeOneStar() {
 		}),
 	
 	++starCount,
-	console.log('star number ' + starCount + ' has been built'),
-	starCrawl();
+	// console.log('star number ' + starCount + ' has been built'),
+	starCrawl(); // set this star into motion
 }
 
+// makeManyStars() runs on the initial page load, and determines
+// the total number of stars on the screen at any time,
+// as well as the initial random position and colors of the first stars
 function makeManyStars() {
 
-	for (var i = 0; i < 1; i++) {
+	for (var i = 0; i < 5; i++) { // this line controls star count
 
 		randoTop = Math.floor(Math.random() * 100),
 		randoLeft = Math.floor(Math.random() * 100),
@@ -70,12 +77,14 @@ function makeManyStars() {
 					'box-shadow':         '0 0 10px 3px ' + randomColor
 			   }),
 
-		starCrawl();
+		starCrawl(); // set each star into motion
 
 	}
 }
 
-// ready set go
+// When page is loaded, do the following...
 $(function() {
-	makeManyStars(); // build stars
+
+	makeManyStars(); // build the first new stars
+
 });
