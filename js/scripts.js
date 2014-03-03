4451e94f0b23eaf94d53e980c305a7eef61b854a
Drew Peifer <peiftronica@gmail.com>
2014-03-03 04:30:03 -0500

    re-arranged functions, made globals, single-star cycle tested till 70+

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,63 @@
+var star = '<div class="new star"></div>',
+	allStars = $('.star'),
+	starField = $('#starfield'),
+	colorA = '#fff',// white
+	colorB = '#ffffee',// yellow
+	colorC = '#ccffff',// blue
+	colors = [colorA, colorB, colorC]
+	starCount = 1;
 
+function starCrawl(starField) {
+	var position = $('.new.star').offset(),
+		starLeft = position.left,
+		fieldWidth = $('#starfield').width();
 
-function makeStars() {
-	var star = '<div class="star"></div>',
-		starField = $('#starfield');
+	$('.new.star').removeClass('new')
+				  .animate({
+			x:((fieldWidth + 100) - starLeft) // 100 is arbitrary
+		}, {
+			duration:5000,
+			easing: 'linear',
+			step: function(now) {
+				if ((now + starLeft) >= fieldWidth) {
+					console.log('its gone now'),
+					$(this).stop()
+						   .remove(),
+					makeOneStar()
+				} else {
+					// never do anything here
+				}
+			}
+		});
+}
+
+function makeOneStar() {
+
+	randoTop = Math.floor(Math.random() * 100),
+	randoLeft = Math.floor(Math.random() * 100),
+	randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+	$(star).appendTo(starField)
+		   .css({
+				'top': randoTop + '%',
+				'left': 0,
+				'background': randomColor,
+				'-moz-box-shadow':    '0 0 10px 3px ' + randomColor,
+				'-webkit-box-shadow': '0 0 10px 3px ' + randomColor,
+				'box-shadow':         '0 0 10px 3px ' + randomColor
+		}),
+	
+	++starCount,
+	console.log('star number ' + starCount + ' has been built'),
+	starCrawl();
+}
+
+function makeManyStars() {
 
 	for (var i = 0; i < 1; i++) {
 
 		randoTop = Math.floor(Math.random() * 100),
 		randoLeft = Math.floor(Math.random() * 100),
-		colorA = '#fff',// white
-		colorB = '#ffffee',// yellow
-		colorC = '#ccffff',// blue
-		colors = [colorA, colorB, colorC],
 		randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 		$(star).appendTo(starField)
@@ -22,68 +68,14 @@ function makeStars() {
 					'-moz-box-shadow':    '0 0 10px 3px ' + randomColor,
 					'-webkit-box-shadow': '0 0 10px 3px ' + randomColor,
 					'box-shadow':         '0 0 10px 3px ' + randomColor
-			   })
+			   }),
+
+		starCrawl();
 
 	}
-
-
-	function starCrawl(starField) {
-		var stars = $('.star');
-
-		stars.each(function(i,obj) {
-			var position = $(this).offset(),
-				starLeft = position.left,
-				fieldWidth = $('#starfield').width();
-
-			$(this).animate({
-					x:((fieldWidth + 100) - starLeft) // 100 is arbitrary
-				}, {
-					duration:5000,
-					step: function(now) {
-						if ((now + starLeft) >= fieldWidth) {
-							console.log('its gone now'),
-							$(this).stop()
-								   .remove(),
-							makeStars();
-						} else {
-							// never do anything here
-						}
-				}
-			});
-
-
-		})
-
-
-		// set varying speeds increments for stars
-		// $('.star').transition({
-		// 	x:'+=1000',
-		// 	duration:10000
-		// })
-		// .filter(':nth-child(2n)')
-		// .stop()
-		// .transition({
-		// 	x:'+=1000',
-		// 	duration:400000
-		// })
-		// .filter(':nth-child(3n)')
-		// .stop()
-		// .transition({
-		// 	x:'+=1000',
-		// 	duration:300000
-		// })
-	}
-
-
-	console.log('star has been built, starting crawl'),
-	starCrawl() // set them in motion
-
 }
-
 
 // ready set go
 $(function() {
-
-	makeStars() // build stars
-
+	makeManyStars(); // build stars
 });
