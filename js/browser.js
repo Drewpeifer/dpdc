c858926188d5b68dcdf13790d1f6e0ecb0fca071
Drew Peifer <peiftronica@gmail.com>
2014-10-13 04:26:55 -0400

    cleaned indentation in browser.js

--- js/browser.js
+++ js/browser.js
@@ -40,7 +40,7 @@ function hideWarning() {
 if (body.hasClass('mobile')) {
     var viewportmeta = document.querySelector('meta[name="viewport"]');
     if (viewportmeta) {
-        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+            viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
         document.body.addEventListener('gesturestart', function () {
             viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
         }, false);
