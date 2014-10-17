98e81d7f33e0359534a2568f31b2b0d4b0a71e83
Drew Peifer <peiftronica@gmail.com>
2014-10-17 00:03:39 -0400

    cleanup and restructure [ipad test]

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,14 @@ $('ul li').click(function() {
 	// when clicking an already-open tab
 
 		$(this).removeClass('active'), // deactivate tab
-		$('.articles #' + activeTab).slideUp(); // close article
+		$('.articles .' + activeTab).slideUp(); // close article
 
 	} else {
 
 		$('.articles div').slideUp(), // hide any active articles
 		$('ul li').removeClass('active'), // deactivate any active tabs
 		$(this).addClass('active'), // activate this tab
-		$('.articles #' + activeTab).slideDown(); // reveal article
+		$('.articles .' + activeTab).slideDown(); // reveal article
 
 	}
 });
