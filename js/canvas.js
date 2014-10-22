c4b3efe3c7a81ca5eebb6d7026042ca8ad27adbf
Drew Peifer <peiftronica@gmail.com>
2014-10-22 05:49:46 -0400

    too much to describe, possibly a mess

--- js/canvas.js
+++ js/canvas.js
@@ -2,32 +2,50 @@ var canvas = document.getElementById('sketch');
 
 if (canvas.getContext) {
 
-    var context = canvas.getContext('2d');
+    var context = canvas.getContext('2d'),
+        helixWidth = $('.canvas').width(); // this is the canvas's wrapper
 
-    var render = function() {
-    
-        var halfW = canvas.width / 2,
-            quartW = halfW / 2,
-            threeQuartW = halfW + quartW,
-            halfH = canvas.height / 2,
-            quartH = halfH / 2,
-            threeQuartH = halfH + quartH;
-    
-    // draw helix
-        context.beginPath();
-        context.moveTo(halfW, 0);
-        context.bezierCurveTo(halfW, 0, threeQuartW, quartH, halfW, halfH);
-        context.bezierCurveTo(halfW, halfH, threeQuartW, threeQuartH, halfW, canvas.height);
-        context.moveTo(halfW, 0);
-        context.bezierCurveTo(halfW, 0, quartW, quartH, halfW, halfH);
-        context.bezierCurveTo(halfW, halfH, quartW, threeQuartH, halfW, canvas.height);
-        context.lineWidth = 6;
-        context.strokeStyle = '#fff';
-        context.stroke();
-    // make it dance
-        
-    };
+    function animate() {
 
-    render();
+        var currentX = helixWidth / 2; // find center
 
+        for (i = 0; i < 10; i += 1) {
+
+            var render = function() {
+                var halfW = helixWidth / 2,
+                    quartW = halfW / 2,
+                    threeQuartW = halfW + quartW,
+                    halfH = canvas.height / 2,
+                    quartH = halfH / 2,
+                    threeQuartH = halfH + quartH;
+
+                    console.log(currentX);
+                // draw helix
+                context.beginPath();
+                context.moveTo(halfW, 0);
+                context.bezierCurveTo(halfW, 0, threeQuartW, quartH, halfW, halfH);
+                context.bezierCurveTo(halfW, halfH, threeQuartW, threeQuartH, halfW, canvas.height);
+                context.lineWidth = 3;
+                context.strokeStyle = 'white';
+                context.stroke();
+                context.moveTo(halfW, 0);
+                context.bezierCurveTo(halfW, 0, quartW, quartH, halfW, halfH);
+                context.bezierCurveTo(halfW, halfH, quartW, threeQuartH, halfW, canvas.height);
+                context.lineWidth = 3;
+                context.strokeStyle = 'white';
+                context.stroke();
+                // make it dance
+                currentX += helixWidth;
+            
+            };
+
+            render();
+
+        }
+
+        render();
+
+    }
+
+    animate();
 }
