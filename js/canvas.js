f0097a8fb8349625912ecc61c49e3f4d9d9490f8
Drew Peifer <peiftronica@gmail.com>
2014-10-22 16:57:42 -0400

    cleaner animation funcs, basic animation present

--- js/canvas.js
+++ js/canvas.js
@@ -2,50 +2,42 @@ var canvas = document.getElementById('sketch');
 
 if (canvas.getContext) {
 
-    var context = canvas.getContext('2d'),
-        helixWidth = $('.canvas').width(); // this is the canvas's wrapper
+    var context = canvas.getContext('2d');
 
-    function animate() {
+    // shim layer with setTimeout fallback
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
 
-        var currentX = helixWidth / 2; // find center
+    var x = 100;
 
-        for (i = 0; i < 10; i += 1) {
+    (function animloop(){
 
-            var render = function() {
-                var halfW = helixWidth / 2,
-                    quartW = halfW / 2,
-                    threeQuartW = halfW + quartW,
-                    halfH = canvas.height / 2,
-                    quartH = halfH / 2,
-                    threeQuartH = halfH + quartH;
+        var render = function() {
 
-                    console.log(currentX);
-                // draw helix
-                context.beginPath();
-                context.moveTo(halfW, 0);
-                context.bezierCurveTo(halfW, 0, threeQuartW, quartH, halfW, halfH);
-                context.bezierCurveTo(halfW, halfH, threeQuartW, threeQuartH, halfW, canvas.height);
-                context.lineWidth = 3;
-                context.strokeStyle = 'white';
-                context.stroke();
-                context.moveTo(halfW, 0);
-                context.bezierCurveTo(halfW, 0, quartW, quartH, halfW, halfH);
-                context.bezierCurveTo(halfW, halfH, quartW, threeQuartH, halfW, canvas.height);
-                context.lineWidth = 3;
-                context.strokeStyle = 'white';
-                context.stroke();
-                // make it dance
-                currentX += helixWidth;
-            
-            };
-
-            render();
+            context.moveTo(x, 0);
+            context.lineTo(x, x);
+            context.strokeStyle = '#fff';
+            context.lineWidth = 10;
+            context.stroke();
 
         }
 
+        if (x <= 400) {
+            x += 20;
+        } else {
+            // do nothing
+        }
+
+        requestAnimFrame(animloop);
         render();
+    })();
+        // place the rAF *before* the render() to assure as close to
+        // 60fps with the setTimeout fallback.
 
-    }
-
-    animate();
 }
