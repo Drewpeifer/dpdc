07493e9bf3ae03d77c47f87e793a30d7d9aa2a34
Drew Peifer <peiftronica@gmail.com>
2014-10-23 01:53:29 -0400

    created helix with some globals, resized canvas to landscape X 8

--- js/canvas.js
+++ js/canvas.js
@@ -15,16 +15,41 @@ if (canvas.getContext) {
     })();
 
     // initial variables here
+    var W = $('.canvas').width(), // this is the wrapper of the canvas
+        H = $('.canvas').height(),
+        halfW = W / 2,
+        quartW = halfW / 2,
+        threeQW = halfW + quartW,
+        halfH = H / 2,
+        quartH = halfH / 2,
+        threeQH = halfH + quartH;
+
+        console.log(W + " / " + H);
+
+    $('canvas').width(W * 8); // make room for 7 more renders
+    $('canvas').height(H); // restrain height to wrapper height
 
     (function animloop(){
 
         var render = function() {
 
-            //draw here
+        // begin custom shape
+        context.beginPath();
+        context.moveTo(halfW, 0);
+        context.bezierCurveTo(halfW, 0, quartW, quartH, halfW, halfH); // top left
+        context.bezierCurveTo(halfW, halfH, threeQW, threeQH, halfW, H); // bottom right
+        context.bezierCurveTo(halfW, H, quartW, threeQH, halfW, halfH); // bottom left
+        context.bezierCurveTo(halfW, halfH, threeQW, quartH, halfW, 0); // top right
+
+        // complete custom shape
+        context.closePath();
+        context.lineWidth = 5;
+        context.fillStyle = '#8ED6FF';
+        //context.fill();
+        context.strokeStyle = '#fff';
+        context.stroke();
 
         }
-
-        // mod loop here
 
         requestAnimFrame(animloop);
         render();
