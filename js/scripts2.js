7e2add1625ff373f1e0f34613947add9ec5c9f24
Drew Peifer <peiftronica@gmail.com>
2016-08-28 22:25:17 -0400

    staging new scripts and styles for revamp

---
+++ js/scripts2.js
@@ -0,0 +1,19 @@
+// navigation
+$('.menu ul li').on('click', function() {
+	var activeTab = $(this).text(); // clicked tab
+
+console.log(activeTab);
+	if ($(this).hasClass('active')) {
+	// when clicking an already-open tab
+
+		//$(this).removeClass('active'), // deactivate tab
+		//$('.drawer .' + activeTab).slideUp(); // close article
+
+	} else {
+
+		$('.drawer div').slideUp(), // hide any active articles
+		$('ul li').removeClass('active'), // deactivate any active tabs
+		$(this).addClass('active'), // activate this tab
+		$('.drawer .' + activeTab).slideDown(); // reveal article
+	};
+});
