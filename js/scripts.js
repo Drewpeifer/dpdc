38057199190f279f92305acc0d3500e7aa6b44ce
Drew Peifer <peiftronica@gmail.com>
2016-09-11 01:45:15 -0400

    replaced primary styles, scripts, and index

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,18 @@
-$(function() {
-	$('.articles div').hide();
-	
-	var bodyW = $('body').width(),
-	    bodyH = $('body').height(),
-	    lamp = $('.lamp'),
-	    wrapper = $('#wrapper');
-	
-	    wrapper.animate({
-	              top:0
-	           }, 5000);
-	
-        lamp.animate({
-                top:-.3 * bodyH,
-                boxShadow: "0 0 300px 300px #d99058"
-            }, 5000);
-	
-	                    
-});
-
-// subsection / drawer functionality
-
-$('ul li').click(function() {
-	var activeTab = $(this).attr('id'); // clicked tab
+// navigation
+$('.menu ul li').on('click', function() {
+	var activeTab = $(this).text();// clicked tab
 
 	if ($(this).hasClass('active')) {
 	// when clicking an already-open tab
 
-		$(this).removeClass('active'), // deactivate tab
-		$('.articles .' + activeTab).slideUp(); // close article
+		//$(this).removeClass('active'),// deactivate tab
+		//$('.drawer .' + activeTab).slideUp();// close article
 
 	} else {
 
-		$('.articles div').slideUp(), // hide any active articles
-		$('ul li').removeClass('active'), // deactivate any active tabs
-		$(this).addClass('active'), // activate this tab
-		$('.articles .' + activeTab).slideDown(); // reveal article
-
-	}
+		$('.drawer div').slideUp(),// hide any active articles
+		$('ul li').removeClass('active'),// deactivate any active tabs
+		$(this).addClass('active'),// activate this tab
+		$('.drawer .' + activeTab).slideDown();// reveal article
+	};
 });
