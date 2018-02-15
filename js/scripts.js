e0baf20b83da105d6afdd3f9223a2b792008ded6
Drew Peifer <peiftronica@gmail.com>
2018-02-15 02:07:26 -0500

    restructured drawers and headers, fixed show/hide func

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 // content hide / show
-$('.content div h2').click(function() {
+$('.headers h2').click(function() {
 	header = $(this);
-	drawer = header.siblings('.drawer');
+	topic = header.attr('class');
+	drawer = $('.drawers .' + topic);
 
 	if (header.hasClass('active')) {
 		// close current section
 		$('h2.active').removeClass('active');
-		$('.drawer.active').removeClass('active').slideUp(100);
+		$('.drawers div.active').removeClass('active').slideUp(100);
 	} else {
+		// deactivate any open sections
 		$('h2.active').removeClass('active');
-		$('.drawer.active').removeClass('active').slideUp(100);
-
+		$('.drawers div.active').removeClass('active').slideUp(100);
+		// make this header and its section active, show section
 		header.addClass('active');
 		drawer.addClass('active').slideDown(400);
 	}
+});
+
+$(function() {
+	content = $('.content');
+
+	content.height(content.width());
 });
