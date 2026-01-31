7816fd26a04213e0b105fdf308a8c70816770c98
Drew Peifer <drew@drewpeifer.com>
2026-01-31 04:03:13 -0500

    replaced widgets with custom gh-repo-widgets

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,42 @@
-const displayContent = (e) => {
-    const link = e.target,
-          topic = link.id,
-          content = document.getElementsByClassName(topic)[0];
-
-    if (link.classList.contains('active')) {
-        // topic is visible, so hide it
-        link.classList.remove('active');
-        document.getElementsByClassName(topic)[0].classList.remove('visible');
-        // show default panel
-        document.getElementById('default').classList.add('visible');
-    } else {
-        // topic is hidden, so show it
-        document.getElementsByClassName('active')[0]?.classList.remove('active');
-        document.getElementsByClassName('visible')[0]?.classList.remove('visible');
-        link.classList.add('active');
-        document.getElementsByClassName(topic)[0].classList.add('visible');
-    }
-}
-
-const controls = document.getElementsByClassName('control');
-Array.from(controls).forEach((control) => {
-    control.addEventListener('click', displayContent);
+GitHubWidget.create({
+    container: '#medialytics-widget',
+    owner: 'drewpeifer',
+    repo: 'medialytics',
+    theme: 'dark'
+});
+GitHubWidget.create({
+    container: '#spr-widget',
+    owner: 'drewpeifer',
+    repo: 'stateprotestrights',
+    theme: 'dark'
+});
+GitHubWidget.create({
+    container: '#widget-widget',
+    owner: 'drewpeifer',
+    repo: 'gh-repo-widget',
+    theme: 'dark'
+});
+GitHubWidget.create({
+    container: '#aframe-widget',
+    owner: 'drewpeifer',
+    repo: 'aframe-solar',
+    theme: 'dark'
+});
+GitHubWidget.create({
+    container: '#apod-widget',
+    owner: 'drewpeifer',
+    repo: 'apod-vue',
+    theme: 'dark'
+});
+GitHubWidget.create({
+    container: '#spacetrade-widget',
+    owner: 'drewpeifer',
+    repo: 'spacetrade',
+    theme: 'dark'
+});
+GitHubWidget.create({
+    container: '#econosim-widget',
+    owner: 'drewpeifer',
+    repo: 'econosim',
+    theme: 'dark'
 });
